ecdc524d8d3fddfd2e6a456171dc6a162a95db27
Daniel Paré <38315881+dnlprjmndr@users.noreply.github.com>
2023-01-13 03:12:35 +0100

    Update TelegramLastest.py

--- TelegramLastest.py
+++ TelegramLastest.py
@@ -6,50 +6,69 @@
 # version: 2.0
 # by www.som-it.com
 
-import requests, subprocess
+import requests, subprocess, math
 
-const MESSAGE_LENGTH = 4096
+# Constant to limit characters of Telegram
+MESSAGE_LENGTH = 4096
 
+# Function String to cut to desired length.
 def exec(command):
     try:
         result = subprocess.Popen(command.split(), shell=True, stdout=subprocess.PIPE).communicate()[0].decode('utf-8')
         return (result)
-    except Exception as err: 
+    except Exception as err:
         print('ERROR exec():', err)
         return(1)
-
+        
+# function String to cut to desired length
 def cut(message):
     iLongMsg = MESSAGE_LENGTH
-    index = 0
+    index = num = 0
     msgTel = []
-    for i in range(len(message) // iLongMsg):
+    
+    for i in range(int(math.ceil(len(message) / iLongMsg))):
         msgTel.append(message[index:iLongMsg])
-        index = index+iLongMsg
+        index += iLongMsg
         iLongMsg += iLongMsg
 
     return msgTel
 
+
+# Send as many messages by telegram as there are partitions of the text
 def send_to_telegram(message):
 
     TOKEN="XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
     ID="NNNNNNNNN"
     apiURL = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
     msgsList = ""
-    #msgsList = exec(message)
-    #if msgsList == 1:
-
+    
     try:
-        if len(message) > MESSAGE_LENGTH:
-           msgsList = cut(message)
-
-           for msg in msgsList:
+        if len(message) < MESSAGE_LENGTH: 
+           msgList = exec(message)
+           response = requests.post(apiURL, json={'chat_id': ID, 'text': message})
+        elif len(message) >= MESSAGE_LENGTH:
+           msgList = cut(message)
+           for msg in msgList:
                response = requests.post(apiURL, json={'chat_id': ID, 'text': msg})
-               #print(response.text)
+        else: 
+            exec(message)
+            
+        print(response)
+        if response.status_code == 200:
+            print("Okay, all good!")
+            return(0)
+        elif response.status_code == 301:
+            print("Ops, the resource has been moved!")
+            return(-1)
+        elif response.status_code == 404:
+            print("Oh no, the resource wasn't found!")
+            return(-2)
         else:
-            response = requests.post(apiURL, json={'chat_id': ID, 'text': message})
-            #print(response)
+            print(response.status_code)        
+
     except Exception as e:
         print("ERR send_to_telegram()" + str(e))
+        return(-1)
 
 #send_to_telegram("asdfhalkdjfdkjfafdshfgdhafha1234567890asdfhalkdjfdkjfafdshfgdhafha")
 #send_to_telegram("df -h")
